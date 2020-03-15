ab1689b21a6441e3450d889625b6734f45628473
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-03-15 20:42:55 +0530

    Backend:Add Register rooute

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,10 +19,42 @@ const User=require("../../models/User")
 // @desc    Registe user
 // @access  Public
 router.post("/",userValidator,runValidation,async(req,res)=>{
-    const error=validationResult(req)
-    if(!error.isEmpty()){
-        return res.status(400).json({error:error.array()})
-    }
+   const {name,email,password}=req.body;
+ try{
+  let user=await User.findOne({email})
+  if(user){
+      return res.json({errors:[{msg:"User already exists"}]});
+  }
+  // gravatar
+  const avatar=gravatar.url(email,{
+    s:"200",
+    r:"pg",
+    d:"mm"
+  })
+  user=await new User({
+    name,email,password
+  })
+
+  const salt=await bcrypt.genSalt(10)
+  user.password=await bcrypt.hash(password,salt);
+  await user.save()
+
+const payload={
+    id:user._id,
+    email:user.email
+}
+  jwt.sign( payload,
+    config.get('jwtSecret'),
+    { expiresIn: 360000 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token:token,user:payload });
+    })
+ }
+ catch(err){
+   console.log(err)
+   res.status(400).json("Server Error")
+ }
 })
 
 
--- validators/user.js
+++ validators/user.js
@@ -2,7 +2,7 @@ const {check}=require("express-validator")
 
 exports.userValidator=[
     check("name","Name is required").not().isEmpty(),
-    check("email","Please enter a valid email").isEmpty(),
+    check("email","Please enter a valid email").isEmail(),
     check("password","Password of atleast 6 characters is required").isLength({min:6})
 
 ]
