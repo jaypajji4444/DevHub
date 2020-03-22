7e79c2057ad0d2c5f9fb839ee1a77d9ab98c75fb
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-03-23 00:14:22 +0530

    client:Add post reducer and action

---
+++ client/src/actions/post.js
@@ -0,0 +1,171 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+    GET_POSTS,
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT,
+    LIKES_ERROR
+} from './types';
+
+
+// Get Psosts
+export const getPost = () => async dispatch => {
+    try {
+        const res = await axios.get("/api/posts");
+        dispatch({
+            type: GET_POSTS,
+            payload: res.data
+        })
+    }
+    catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: err
+        })
+    }
+}
+
+// Add Like
+export const addLike = (id) => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${id}`);
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data }
+        })
+    }
+    catch (err) {
+        dispatch({
+            type: LIKES_ERROR,
+            payload: err
+        })
+    }
+}
+
+// Delete like
+
+export const removeLike = (id) => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${id}`)
+        dispatch({
+            type: LIKES_ERROR,
+            payload: { id, likes: res.data }
+        })
+    }
+    catch (err) {
+        dispatch({
+            type: LIKES_ERROR,
+            payload
+        })
+    }
+}
+
+
+// Delete post
+export const deletePost = id => async dispatch => {
+    try {
+        await axios.delete(`/api/posts/${id}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: id
+        });
+        dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: err
+        });
+    }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    try {
+        const res = await axios.post('/api/posts', formData, config);
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:err
+        });
+    }
+};
+
+// Get post
+export const getPost = id => async dispatch => {
+    try {
+        const res = await axios.get(`/api/posts/${id}`);
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: err
+        });
+    }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post(
+            `/api/posts/comment/${postId}`,
+            formData,
+            config
+        );
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:err
+        });
+    }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+    try {
+        await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+        dispatch(setAlert('Comment Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: err
+        });
+    }
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -23,6 +23,7 @@ export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 
 export const GET_POSTS = 'GET_POSTS';
 export const GET_POST = 'GET_POST';
+export const LIKES_ERROR="LIKES_ERROE"
 export const POST_ERROR = 'POST_ERROR';
 export const UPDATE_LIKES = 'UPDATE_LIKES';
 export const DELETE_POST = 'DELETE_POST';
